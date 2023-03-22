4b34070a00ea1ea7fffa67bb4f74190517eec1c3
Felipe Menanteau <felipe@illinois.edu>
2023-03-22 15:44:04 -0500

    More updates to docker recipe entrypoint

---
+++ docker/startup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Taken for LSSTTS Dockefile
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+# on callback, kill the last background process and execute term_handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+echo "Starting env"
+source /opt/spt/setSPTEnv.sh
+
+args=("$@")
+exec "${args[@]}"
